56c6a0e4239aae550cc911bfa9f108fa069eca19
Abhay Sagar <abhaysagar818@gmail.com>
2024-01-02 20:36:32 +0530

    CURD opretion

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -56,4 +56,52 @@ $description = $validatedData['description'];
         $product = Product::where('id',$id)->first();
         return view('edit',['product' => $product]);
     }
+
+    public function update(Request $request ,$id){
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'image' => 'nullable|image', // Assuming 'image' is a file input for image upload
+            'password' => 'required|string|min:8|regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/',
+            'email' => 'required|email',
+            'description' => 'required|',
+        ]);
+
+
+        // Access individual validated fields
+        $name = $validatedData['name'];
+        // $image = $validatedData['image'];
+        $password = $validatedData['password'];
+        $email = $validatedData['email'];
+        $description = $validatedData['description'];
+
+        $product = Product::where('id',$id)->first();
+        if(isset($request->image))
+        {
+               $imagename = time().'.'.$request->image->extension();
+               $request->image->move(public_path('product'),$imagename);
+               $product->image = $imagename;
+        }
+         
+        $product->name = $request->name;
+        $product->email = $request->email;
+        $product->password = Hash::make($request->input('password'));
+        $product->description = $request->description;
+        $product->save();
+        return redirect()->back()->with('success',"Your Registration form Update Successfully!");
+ 
+    }
+
+    public function delete($id)
+    {
+        $product = Product::find($id);
+    
+        if (!$product) {
+            return redirect()->back()->with('error', 'Product not found.');
+        }
+    
+        $product->delete();
+    
+        return redirect()->back()->with('success', 'Product deleted successfully.');
+    }
+    
 }
