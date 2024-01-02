800a251e109094ca8149eb8fa6eb9fd442730822
Abhay Sagar <abhaysagar818@gmail.com>
2024-01-02 17:29:17 +0530

    CURD project

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\Product;
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all();
+        return view('product');
+    }
+
+    public function create()
+    {
+        $products = Product::all();
+        return view('create',compact('products'));
+    }
+
+    public function store(Request $request)
+    {
+        //validation form
+        $validatedData = $request->validate([
+            'name' => 'required',
+            'image' => 'required|image', // Assuming 'image' is a file input for image upload
+            'password' => 'required|string|min:8|regex:/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d).+$/',
+            'email' => 'required|email||unique:products',
+            'description' => 'required|',
+        ]);
+
+
+        // Access individual validated fields
+$name = $validatedData['name'];
+$image = $validatedData['image'];
+$password = $validatedData['password'];
+$email = $validatedData['email'];
+$description = $validatedData['description'];
+
+
+        // dd($request->all());
+        $imagename = time().'.'.$request->image->extension();
+        $request->image->move(public_path('product'),$imagename);
+        // dd($imagename);
+        $product = new Product;
+        $product->image = $imagename;
+        $product->name = $request->name;
+        $product->email = $request->email;
+        $product->password = Hash::make($request->input('password'));
+        $product->description = $request->description;
+        $product->save();
+        return redirect()->back()->with('success',"Your Registration form Successfully Submited!");
+    }
+
+    public function edit($id)
+    {
+        $product = Product::where('id',$id)->first();
+        return view('edit',['product' => $product]);
+    }
+}
